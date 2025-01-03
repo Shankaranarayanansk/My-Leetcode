9fdf16f17f2c2c52887acf4fc83c9f6f09583323
Shankaranarayanan <127618183+Shankaranarayanansk@users.noreply.github.com>
2025-01-03 20:46:46 +0530

    Time: 7 ms (16.5%), Space: 8.5 MB (86.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,12 +4,13 @@ public:
         if (x < 0 || (x % 10 == 0 && x != 0)) 
             return false;
         
-        int rev = 0, ori = x;
+        long rev = 0;
+        int ori = x;
 
         while (x != 0) {
             int a = x % 10;
-            if(rev>INT_MAX/10)
-            return false;
+            // if(rev>INT_MAX/10)
+            // return false;
             rev = rev * 10 + a;
             x /= 10;
         }
