1038bc1b4e0143284d3b9ecc2d12781caf203f7e
Shankaranarayanan <127618183+Shankaranarayanansk@users.noreply.github.com>
2025-03-21 13:06:37 -0400

    Time: 62 ms (55.85%), Space: 46.6 MB (70.53%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        Set<String> availableSupplies = new HashSet<>(Arrays.asList(supplies));
+        Map<String, List<String>> ingredientToRecipes = new HashMap<>();
+        Map<String, Integer> inDegree = new HashMap<>();
+        Map<String, List<String>> recipeToIngredients = new HashMap<>();
+
+        for (int i = 0; i < recipes.length; i++) {
+            String recipe = recipes[i];
+            List<String> recipeIngredients = ingredients.get(i);
+            recipeToIngredients.put(recipe, recipeIngredients);
+            inDegree.put(recipe, recipeIngredients.size());
+
+            for (String ingredient : recipeIngredients) {
+                if (!ingredientToRecipes.containsKey(ingredient)) {
+                    ingredientToRecipes.put(ingredient, new ArrayList<>());
+                }
+                ingredientToRecipes.get(ingredient).add(recipe);
+            }
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        for (String supply : supplies) {
+            queue.add(supply);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+
+            if (recipeToIngredients.containsKey(current)) {
+                result.add(current);
+            }
+
+            if (ingredientToRecipes.containsKey(current)) {
+                for (String dependentRecipe : ingredientToRecipes.get(current)) {
+                    inDegree.put(dependentRecipe, inDegree.get(dependentRecipe) - 1);
+                    if (inDegree.get(dependentRecipe) == 0) {
+                        queue.add(dependentRecipe);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
